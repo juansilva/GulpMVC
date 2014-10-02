b458a134c363ceab192b7834d5498a5cc207ad41
Juan Silva <juansanuy@gmail.com>
2014-10-01 23:07:43 -0300

    Adds js task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,30 @@
 var gulp = require('gulp')
+,	concat = require('gulp-concat')
+,	uglify = require('gulp-uglify')
+
+
+///////////////////////
+/// Javascript task ///
+///////////////////////
+var javascript_globs = [
+	'bower_components/jquery/jquery.js'
+,	'bower_components/underscore/underscore.js'
+,	'bower_components/backbone/backbone.js'
+,	'bower_components/backbone.localStorage/backbone.localStorage.js'
+,	'dist/templates.js'
+,	'js/models/todo.js'
+,	'js/collections/todos.js'
+,	'js/views/todo-view.js'
+,	'js/views/app-view.js'
+,	'js/routers/router.js'
+,	'js/app.js'
+]
+gulp.task('js', function()
+{
+	return gulp.src(javascript_globs)
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest("dist"));
+});
 
 
 gulp.task('default', function()
